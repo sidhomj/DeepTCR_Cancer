b3492c0095cf6409cde89b8c45ea8c273145f921
jsidhom1 <jsidhom1@jhmi.edu>
2020-12-15 12:00:52 -0500

    init

--- scripts/Sup_Human_HLA.py
+++ scripts/Sup_Human_HLA.py
@@ -2,9 +2,13 @@
 
 from DeepTCR.DeepTCR import DeepTCR_WF
 import numpy as np
+import os
 
+gpu = 3
+os.environ["CUDA DEVICE ORDER"] = 'PCI_BUS_ID'
+os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu)
 
-DTCR = DeepTCR_WF('HLA',device=3)
+DTCR = DeepTCR_WF('HLA')
 DTCR.Get_Data(directory='../Data',Load_Prev_Data=False,
                aa_column_beta=1,count_column=2,v_beta_column=7,d_beta_column=14,j_beta_column=21,data_cut=1.0,
               hla='../Data/HLA_Ref_sup_AB.csv')
@@ -20,6 +24,6 @@ seeds = np.array(range(folds))
 graph_seed = 0
 #
 DTCR.Monte_Carlo_CrossVal(folds=folds,LOO=LOO,epochs_min=epochs_min,size_of_net=size_of_net, num_concepts=num_concepts,
-                          combine_train_valid=True,hinge_loss_t=hinge_loss_t,train_loss_min=train_loss_min,
-                          use_only_hla=True,seeds=seeds,graph_seed=graph_seed)
+                          combine_train_valid=True,hinge_loss_t=hinge_loss_t,train_loss_min=train_loss_min,seeds=seeds,
+                          graph_seed=graph_seed,use_only_hla=True)
 DTCR.DFs_pred['crpr'].to_csv('sample_hla.csv',index=False)
--- scripts/Sup_Human_TCR.py
+++ scripts/Sup_Human_TCR.py
@@ -2,9 +2,12 @@
 
 from DeepTCR.DeepTCR import DeepTCR_WF
 import numpy as np
+import os
+gpu = 2
+os.environ["CUDA DEVICE ORDER"] = 'PCI_BUS_ID'
+os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu)
 
-
-DTCR = DeepTCR_WF('TCR',device=1)
+DTCR = DeepTCR_WF('TCR')
 DTCR.Get_Data(directory='../Data',Load_Prev_Data=False,
                aa_column_beta=1,count_column=2,v_beta_column=7,d_beta_column=14,j_beta_column=21,data_cut=1.0,
               hla='../Data/HLA_Ref_sup_AB.csv')
@@ -21,5 +24,6 @@ graph_seed = 0
 
 DTCR.use_hla = False
 DTCR.Monte_Carlo_CrossVal(folds=folds,LOO=LOO,epochs_min=epochs_min,size_of_net=size_of_net, num_concepts=num_concepts,
-                          combine_train_valid=True,hinge_loss_t=hinge_loss_t,train_loss_min=train_loss_min,seeds=seeds,graph_seed=graph_seed)
+                          combine_train_valid=True,hinge_loss_t=hinge_loss_t,train_loss_min=train_loss_min,seeds=seeds,
+                          graph_seed=graph_seed)
 DTCR.DFs_pred['crpr'].to_csv('sample_tcr.csv',index=False)

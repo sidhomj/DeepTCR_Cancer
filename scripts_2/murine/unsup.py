244de698364a3e0eb12fd2eb744b76c78bd5b2a3
jsidhom1 <jsidhom1@jhmi.edu>
2021-01-20 02:36:20 -0500

    init

--- scripts_2/murine/unsup.py
+++ scripts_2/murine/unsup.py
@@ -27,26 +27,40 @@ for ii,cl in enumerate(DTCR.lb.classes_,0):
     ax[ii].set_yticks([])
     ax[ii].set_title(cl)
 
+from scipy.stats import gaussian_kde
+def GKDE(x,y,z=None):
+    xy = np.vstack([x, y])
+    kernel = gaussian_kde(xy,weights=z)
+    z = kernel(xy)
+    r = np.argsort(z)
+    x ,y, z = x[r], y[r], z[r]
+    return x,y,z,kernel,r
+
+classes = ['Control','RT','9H10','Combo']
 plt.scatter(X_2[:,0],X_2[:,1])
 xlim = plt.xlim()
 ylim = plt.ylim()
 plt.close()
 fig,ax = plt.subplots(4,5,figsize=(10,10))
-for ii,cl in enumerate(DTCR.lb.classes_,0):
+for ii,cl in enumerate(classes,0):
     df_class = df_preds[df_preds['label']==cl]
     for jj,s in enumerate(np.unique(df_class['sample']),0):
         df_sample = df_class[df_class['sample']==s]
+        df_sample.sort_values(by=cl,ascending=False,inplace=True)
+        df_sample = df_sample.iloc[0:int(np.round(len(df_sample)*0.25))]
         c = np.array(df_sample[cl])
         s = np.array(df_sample['freq'])
         idx = np.argsort(c)
         x,y = np.array(df_sample['x']),np.array(df_sample['y'])
-        ax[ii,jj].scatter(x[idx], y[idx], c=c[idx], cmap='jet', s=s[idx] * 1000)
+        # ax[ii,jj].scatter(x[idx], y[idx], c=c[idx], cmap='jet', s=s[idx] * 1000)
+        x,y,z,_,_ = GKDE(x,y,s)
+        ax[ii,jj].scatter(x, y,c=z, cmap='jet',s=10)
         ax[ii,jj].set_xticks([])
         ax[ii,jj].set_yticks([])
         ax[ii,jj].set_xlim(xlim)
         ax[ii,jj].set_ylim(ylim)
         if jj == 0:
-            ax[ii,jj].set_ylabel(cl)
+            ax[ii,jj].set_ylabel(cl,fontsize=18)
 plt.tight_layout()
 
 

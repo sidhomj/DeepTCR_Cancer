a399c993e5d86c2731fd76ae46804c20a19ac294
jsidhom1 <jsidhom1@jhmi.edu>
2021-01-20 23:45:38 -0500

    init

---
+++ scripts_2/human/inf_post.py
@@ -0,0 +1,49 @@
+from DeepTCR.DeepTCR import DeepTCR_WF
+import numpy as np
+import pickle
+import os
+from sklearn.metrics import roc_auc_score,roc_curve
+
+gpu = 2
+os.environ["CUDA DEVICE ORDER"] = 'PCI_BUS_ID'
+os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu)
+
+DTCR = DeepTCR_WF('load')
+DTCR.Get_Data(directory='../../Data_Post',Load_Prev_Data=False,
+               aa_column_beta=1,count_column=2,v_beta_column=7,d_beta_column=14,j_beta_column=21,data_cut=1.0,
+              hla='../../Data_Post/HLA_Ref_sup_AB.csv')
+beta_sequences = DTCR.beta_sequences
+v_beta = DTCR.v_beta
+d_beta = DTCR.d_beta
+j_beta = DTCR.j_beta
+hla = DTCR.hla_data_seq
+sample_id = DTCR.sample_id
+counts = DTCR.counts
+
+DTCR = DeepTCR_WF('HLA_TCR')
+DTCR.Sample_Inference(sample_labels=sample_id,beta_sequences=beta_sequences,v_beta=v_beta,d_beta=d_beta,j_beta=j_beta,
+                      hla=hla,counts=counts)
+
+import glob
+files = glob.glob('../../Data_Post/crpr/*.tsv')
+filescrpr = [os.path.basename(x) for x in files]
+labelcrpr = [['crpr']*len(files)]
+
+files = glob.glob('../../Data_Post/sdpd/*.tsv')
+filessdpd = [os.path.basename(x) for x in files]
+labelsdpd = [['sdpd']*len(files)]
+
+files = np.hstack([filescrpr,filessdpd])
+labels = np.squeeze(np.hstack([labelcrpr,labelsdpd]),0)
+label_dict = dict(zip(files,labels))
+
+df_pred = DTCR.Inference_Pred_Dict['crpr']
+df_pred['label'] = df_pred['Samples'].map(label_dict)
+df_pred['label_bin'] = 0.0
+df_pred['label_bin'][df_pred['label']=='crpr']=1.0
+roc_auc_score(df_pred['label_bin'],df_pred['Pred'])
+
+
+
+import seaborn as sns
+sns.violinplot(data=df_pred,x='label',y='Pred',cut=0)
--- scripts_2/murine/seq_logos.py
+++ scripts_2/murine/seq_logos.py
@@ -9,14 +9,14 @@ df_preds = pd.read_csv('preds.csv')
 class_sel = 'Control'
 df_preds.sort_values(by=class_sel,inplace=True,ascending=False)
 
-num = 25
+num = 10
 beta_sequences = np.array(df_preds['beta_sequences'])[0:num]
 v_beta = np.array(df_preds['v_beta'])[0:num]
 d_beta = np.array(df_preds['d_beta'])[0:num]
 j_beta = np.array(df_preds['j_beta'])[0:num]
 
-models = np.random.choice(range(100),10,replace=False)
+models = np.random.choice(range(100),25,replace=False)
 models = np.array(['model_'+str(x) for x in models])
 DTCR_WF.Residue_Sensitivity_Logo(beta_sequences=beta_sequences,v_beta=v_beta,d_beta=d_beta,j_beta=j_beta,
-                                 class_sel=class_sel,background_color='black',Load_Prev_Data=False,
+                                 class_sel=class_sel,background_color='white',Load_Prev_Data=True,
                                  models=models,figsize=(3,4),min_size=0.75)

e4801b84f2e82fb0526e818d4c325c0b30eadc07
jsidhom1 <jsidhom1@jhmi.edu>
2021-01-20 21:09:21 -0500

    init

--- scripts_2/human/CM038_Qualitative_U.py
+++ scripts_2/human/CM038_Qualitative_U.py
@@ -30,7 +30,7 @@ os.environ["CUDA DEVICE ORDER"] = 'PCI_BUS_ID'
 os.environ["CUDA_VISIBLE_DEVICES"] = "6"
 
 DTCR = DeepTCR_WF('Human_TIL',device='/device:GPU:0')
-DTCR.Get_Data(directory='../Data',Load_Prev_Data=False,
+DTCR.Get_Data(directory='../../Data',Load_Prev_Data=False,
                aa_column_beta=1,count_column=2,v_beta_column=7,d_beta_column=14,j_beta_column=21,data_cut=1.0,
               hla='../Data/HLA_Ref_sup_AB.csv')
 

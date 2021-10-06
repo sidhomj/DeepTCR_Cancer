a232bf9e55c8c5aa63d6ed31cc7cc17c022ad529
sidhomj <jsidhom1@jhmi.edu>
2021-10-06 04:50:50 -0400

    init

--- scripts/sc_analysis/score_tcrs.py
+++ scripts/sc_analysis/score_tcrs.py
@@ -1,12 +1,12 @@
 from DeepTCR.DeepTCR import DeepTCR_WF
 import pandas as pd
 import numpy as np
-def process_gene(chain,df_merge):
-    temp = df_merge['TRB'+chain+'_1'].str.split('TRB'+chain,expand=True)[1].str.split('-',expand=True)
+def process_gene(gene,chain,df_merge):
+    temp = df_merge['TR'+chain+gene+'_1'].str.split('TR'+chain+gene,expand=True)[1].str.split('-',expand=True)
     temp.fillna(value=1,inplace=True)
     temp[0] = temp[0].astype(int).map("{:02}".format)
     temp[1] = temp[1].astype(int).map("{:02}".format)
-    temp[2] = 'TCRB' + chain + temp[0].astype(str) + '-'+temp[1].astype(str)
+    temp[2] = 'TCR'+chain + gene + temp[0].astype(str) + '-'+temp[1].astype(str)
     return temp[2]
 
 df_tcr = pd.read_csv('../../SC/scTCRs.csv')
@@ -15,8 +15,8 @@ df_tcr['patient'] = df_tcr['patient'].str.split('.',expand=True)[0]
 
 df_hla = pd.read_csv('../../SC/hla.csv')
 df_merge = pd.merge(df_tcr,df_hla,on='patient')
-df_merge['TRBV_1'] = process_gene('V',df_merge)
-df_merge['TRBJ_1'] = process_gene('J',df_merge)
+df_merge['TRBV_1'] = process_gene('V','B',df_merge)
+df_merge['TRBJ_1'] = process_gene('J','B',df_merge)
 df_merge['TRBD_1'][df_merge['TRBD_1']=='None'] = 'nan'
 
 beta_sequences = np.array(df_merge['CDR3B_1'])
--- scripts/sc_analysis/train_tumor_spec.py
+++ scripts/sc_analysis/train_tumor_spec.py
@@ -5,6 +5,7 @@ from DeepTCR.DeepTCR import DeepTCR_SS
 
 df_scores = pd.read_csv('tcrs_scored.csv')
 df_scores.rename(columns={'final.clonotype.family':'TCR clonotype family'},inplace=True)
+df_scores.dropna(subset=['CDR3A_1'],inplace=True)
 
 df_label = pd.read_csv('../../SC/TCR clones tumor specificity.csv')
 df_label = pd.melt(df_label)
@@ -21,13 +22,23 @@ beta_sequences = np.array(df_merge['CDR3B_1'])
 v_beta = np.array(df_merge['TRBV_1'])
 d_beta = np.array(df_merge['TRBD_1'])
 j_beta = np.array(df_merge['TRBJ_1'])
+alpha_sequences = np.array(df_merge['CDR3A_1'])
+v_alpha = np.array(df_merge['TRAV_1'])
+j_alpha = np.array(df_merge['TRAJ_1'])
 class_labels = np.array(df_merge['label'])
 
 DTCR = DeepTCR_SS('tumor_s')
-DTCR.Load_Data(beta_sequences=beta_sequences,v_beta=v_beta,j_beta=j_beta,class_labels=class_labels)
-# DTCR.Load_Data(beta_sequences=beta_sequences,class_labels=class_labels)
-DTCR.Monte_Carlo_CrossVal(folds=10)
+DTCR.Load_Data(beta_sequences=beta_sequences,v_beta=v_beta,j_beta=j_beta,
+               alpha_sequences=alpha_sequences,v_alpha=v_alpha,j_alpha=j_alpha,
+               class_labels=class_labels)
+DTCR.Monte_Carlo_CrossVal(folds=100)
 DTCR.Representative_Sequences(make_seq_logos=False,top_seq=50)
 DTCR.AUC_Curve()
-DTCR.Residue_Sensitivity_Logo(beta_sequences=np.array(DTCR.Rep_Seq['MAA']['beta'])[0:10],
-                              class_sel='MAA')
+DTCR.Residue_Sensitivity_Logo(beta_sequences=np.array(DTCR.Rep_Seq['MAA']['beta'])[0:25],
+                              alpha_sequences=np.array(DTCR.Rep_Seq['MAA']['alpha'])[0:25],
+                              class_sel='MAA',models=['model_'+str(x) for x in np.random.choice(range(100),10)],
+                              background_color='black',
+                              figsize=(3,8),
+                              Load_Prev_Data=False,
+                              min_size=0.5)
+

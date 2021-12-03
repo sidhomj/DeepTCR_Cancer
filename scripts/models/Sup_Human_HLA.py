e9c798d430171ea2a5395b536658e5530a36f412
sidhomj <jsidhom1@jhmi.edu>
2021-12-02 20:15:22 -0500

    init

--- scripts/models/Sup_Human_HLA.py
+++ scripts/models/Sup_Human_HLA.py
@@ -9,7 +9,7 @@ os.environ["CUDA DEVICE ORDER"] = 'PCI_BUS_ID'
 os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu)
 
 DTCR = DeepTCR_WF('HLA')
-DTCR.Get_Data(directory='../../Data/bulk_tcr/pre',Load_Prev_Data=True,
+DTCR.Get_Data(directory='../../Data/bulk_tcr/pre',Load_Prev_Data=False,
                aa_column_beta=1,count_column=2,v_beta_column=7,d_beta_column=14,j_beta_column=21,data_cut=1.0,
               hla='../../Data/bulk_tcr/pre/HLA_Ref_sup_AB.csv')
 

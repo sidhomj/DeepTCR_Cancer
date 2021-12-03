5b980abe3a7bcf3a267b0087b394ccdcb1b060a5
sidhomj <jsidhom1@jhmi.edu>
2021-12-03 02:52:32 -0500

    init

--- scripts/unsup/CM038_Qualitative_U.py
+++ scripts/unsup/CM038_Qualitative_U.py
@@ -63,7 +63,7 @@ def hist2d_denisty_plot(h, X, Y, ax, log_transform=False, gaussian_sigma=-1, nor
 os.environ["CUDA DEVICE ORDER"] = 'PCI_BUS_ID'
 os.environ["CUDA_VISIBLE_DEVICES"] = "6"
 
-DTCR = DeepTCR_WF('HLA_TCR')
+DTCR = DeepTCR_WF('../models/HLA_TCR')
 DTCR.Get_Data(directory='../../Data',Load_Prev_Data=True,
                aa_column_beta=1,count_column=2,v_beta_column=7,d_beta_column=14,j_beta_column=21,data_cut=1.0,
               hla='../../Data/HLA_Ref_sup_AB.csv')

8192dc1963b12d7db753162718e77a3ad7c71b43
sidhomj <jsidhom1@jhmi.edu>
2021-12-02 22:09:39 -0500

    init

---
+++ scripts/antigen/umaps.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib
+
+df_scores = pd.read_csv('oliveira_tcrs_scored.csv')
+df_scores.rename(columns={'final.clonotype.family':'TCR clonotype family'},inplace=True)
+
+df = pd.read_csv('../../SC/antigen.csv')
+df = pd.melt(df,id_vars=[df.columns[0]],
+               value_vars=['NeoAg', 'Cathegory','norm TCR activation', 'Avidity'])
+
+df = df[df['variable']=='Cathegory']
+df = df[['TCR clonotype family','value']]
+df.rename(columns={'value':'label'},inplace=True)
+df.reset_index(drop=True,inplace=True)
+df['label'][df['label'] == 'Multi'] = 'NeoAg'
+df2 = df
+df_merge = pd.merge(df_scores,df,on='TCR clonotype family')
+label_dict = {'MAA':'Tumor Specific', 'NeoAg':'Tumor Specific', 'Viral':'Virus Specific'}
+df_merge['label'] = df_merge['label'].map(label_dict)
+df_umap = pd.read_csv('../../SC/umap.csv')
+df_umap.rename(columns={'Unnamed: 0':'cell.barcode 2'},inplace=True)
+df_merge = pd.merge(df_merge,df_umap,on='cell.barcode 2')
+labels = np.unique(df_merge['label'])
+num_windows = len(labels)+1
+fig,ax = plt.subplots(1,num_windows,figsize=(num_windows*3,3))
+for ii,g in enumerate(labels,0):
+    ax[ii].scatter(df_umap['UMAP_1'],df_umap['UMAP_2'],c='grey',s=1)
+    idx = df_merge['label'] == g
+    ax[ii].scatter(df_merge['UMAP_1'][idx],df_merge['UMAP_2'][idx],c='r',s=1)
+    ax[ii].set_title(g)
+    ax[ii].set_xticks([])
+    ax[ii].set_yticks([])
+
+idx = np.argsort(np.array(df_merge['pred']))
+ii = ii+1
+im = ax[ii].scatter(df_merge['UMAP_1'][idx],df_merge['UMAP_2'][idx],c=df_merge['pred'][idx],s=5,cmap='jet')
+fig.colorbar(im)
+ax[ii].set_title('P(Response)')
+ax[ii].set_xticks([])
+ax[ii].set_yticks([])
+plt.tight_layout()
+plt.savefig('umap.png',dpi=600)
+
+# plt.tight_layout()
+
+# low_color = 'blue'
+# high_color = 'red'
+# cvals = [-1,1]
+# colors = [low_color,high_color]
+# norm = plt.Normalize(min(cvals), max(cvals))
+# tuples = list(zip(map(norm, cvals), colors))
+# cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", tuples)
---
+++ scripts/antigen/vioiln_oliveira.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from DeepTCR.DeepTCR import DeepTCR_SS
+import matplotlib.pyplot as plt
+import pickle
+import os
+from scipy.stats import gaussian_kde
+def GKDE(x,y,z=None):
+    xy = np.vstack([x, y])
+    kernel = gaussian_kde(xy,weights=z)
+    z = kernel(xy)
+    r = np.argsort(z)
+    x ,y, z = x[r], y[r], z[r]
+    return x,y,z,kernel,r
+
+df_scores1 = pd.read_csv('oliveira_tcrs_scored.csv')
+df_scores2 = pd.read_csv('../sc_analysis/tcrs_scored.csv')
+df_merge = pd.merge(df_scores1,df_scores2,on='cell.barcode')
+df_merge = df_merge[['CDR3B_1_x','CDR3B_1_y','pred_x','pred_y']]
+df_merge['diff'] = df_merge['pred_x']-df_merge['pred_y']
+df_merge.sort_values(by='diff',ascending=False,inplace=True)
+df_comp = pd.DataFrame()
+# df_comp['1'] =
+
+df_scores.rename(columns={'final.clonotype.family':'TCR clonotype family'},inplace=True)
+
+df_antigen = pd.read_csv('../../SC/antigen.csv')
+df_antigen['Cathegory'][df_antigen['Cathegory'] == 'Multi'] = 'NeoAg'
+
+train_list = []
+
+df_viral = df_antigen[df_antigen['Cathegory'] == 'Viral']
+df_viral['label'] = df_viral['NeoAg']
+train_list.append(df_viral)
+
+df_maa = df_antigen[df_antigen['Cathegory'] == 'MAA']
+df_maa = df_maa[~df_maa['NeoAg'].isin(['PMEL589-603','Pmel pool','TYR207-215',
+                                       'PMEL154-162','TYR514-524','PMEL209-217'])]
+df_maa['label'] = df_maa['NeoAg']
+train_list.append(df_maa)
+
+df_neo = df_antigen[df_antigen['Cathegory']=='NeoAg']
+df_neo['label'] = df_neo['Cathegory']
+train_list.append(df_neo)
+
+df_antigen = pd.concat(train_list)
+
+df_merge = pd.merge(df_scores,df_antigen,on='TCR clonotype family')
+df_merge.drop_duplicates(subset=['TCR clonotype family','CDR3B_1'],inplace=True)
+
+# pal = {'NeoAg':'b',''}
+sns.violinplot(data=df_merge,x='label',y='pred',
+            order = list(df_merge.groupby(['label']).mean()['pred'].sort_values().index),
+               cut=0)
+plt.ylabel('P(Response)',fontsize=24)
+plt.xlabel('')

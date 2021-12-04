f3088be75b09f65b031baaafc5e0738ce554847b
sidhomj <jsidhom1@jhmi.edu>
2021-12-04 14:51:17 -0500

    init

--- scripts/dynamics/sample_dynamics.py
+++ scripts/dynamics/sample_dynamics.py
@@ -19,38 +19,31 @@ for ii,c in enumerate(cuts,0):
     except:
         continue
 
-# df = df[(df['freq_pre'] > 0.001) | (df['freq_post']>.001)]
-df = df[df['freq_post']>0]
-df['fc_w'] = df['fc']*df['freq_pre']
-df['delta_w'] = df['delta']*df['freq_pre']
-df['abs_delta_w'] = df['abs_delta']*df['freq_pre']
-df_agg = df.groupby(['sample','seq_type','gt']).agg({'delta':np.sum,'abs_delta':np.sum,
-                                                     'freq_pre':np.sum,'freq_post':np.sum,
-                                                     'fc':'mean','fc_w':'sum','abs_delta_w':'sum',
-                                                     'delta_w':'sum'}).reset_index()
-# df_agg['abs_delta_w'] = np.abs(df_agg['delta_w'])
-# df_agg['abs_delta_norm'] = df_agg['abs_delta']/df_agg['freq_pre']
+df_pre = df[df['freq_pre']>0]
+df_agg = df_pre.groupby(['sample','seq_type','gt']).agg({'delta':np.sum}).reset_index()
 
 plt.figure()
 g = sns.boxplot(data=df_agg,hue='gt',y='delta',x='seq_type',showfliers=False,order=order,showmeans=True)
 plt.xlabel('')
-plt.ylabel('Δ',fontsize=26)
+plt.ylabel('Δ Frequency',fontsize=26)
 plt.xticks([])
 ax = plt.gca()
 g.legend_.set_title(None)
 plt.legend(fontsize='x-large')
 plt.tight_layout()
-plt.savefig('sample_delta.png',dpi=1200)
+plt.savefig('delta_pre_sample.png',dpi=1200)
+
+df_post = df[df['freq_post']>0]
+df_agg = df_post.groupby(['sample','seq_type','gt']).agg({'delta':np.sum}).reset_index()
 
 plt.figure()
 pal = {'crpr':'royalblue','sdpd':'red'}
-g = sns.boxplot(data=df_agg,hue='gt',y='fc_w',x='seq_type',showfliers=False,order=order,palette=pal)
-# g = sns.swarmplot(data=df_agg,hue='gt',y='abs_delta',x='seq_type',order=order)
+g = sns.boxplot(data=df_agg,hue='gt',y='delta',x='seq_type',showfliers=False,order=order,palette=pal)
 plt.xlabel('')
-plt.ylabel('Absolute Δ',fontsize=26)
+plt.ylabel('Δ Frequency',fontsize=26)
 plt.xticks([])
 ax = plt.gca()
 g.legend_.set_title(None)
 plt.legend(fontsize='x-large')
 plt.tight_layout()
-plt.savefig('sample_abs_delta.png',dpi=1200)
+plt.savefig('delta_post_sample.png',dpi=1200)
